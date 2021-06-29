b129f750a11f0d96f78555565f57087dace4d61c
Jake Ruesink <jaruesink@gmail.com>
2021-06-29 16:56:22 -0500

    init commit

---
+++ gatsby-site/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home: React.FC<{}> = () => {
+  return <h1>Initial Test Site for Lambda Curry's Gatsby Themes</h1>;
+};
+
+export default Home;
---
+++ gatsby-theme/gatsby-config.js
@@ -0,0 +1,13 @@
+const path = require("path")
+
+
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: path.join(__dirname, "src/pages"),
+      },
+    },
+  ],
+}
