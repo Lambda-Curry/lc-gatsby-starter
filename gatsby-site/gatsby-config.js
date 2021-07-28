1ca67be54ee46bd06e907bb456b880fee4ce771c
Jake Ruesink <jaruesink@gmail.com>
2021-07-28 16:05:32 -0500

    broken state

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
   plugins: [
     `gatsby-plugin-image`,
@@ -8,13 +12,13 @@ module.exports = {
           title: 'Test site'
         }
       }
-    },
-    {
-      resolve: `@lambdacurry/gatsby-theme-directus`,
-      options: {
-        url: process.env.DIRECTUS_URL,
-        authToken: process.env.DIRECTUS_AUTH_TOKEN
-      }
     }
+    // {
+    //   resolve: `@lambdacurry/gatsby-theme-directus`,
+    //   options: {
+    //     url: process.env.DIRECTUS_URL,
+    //     authToken: process.env.DIRECTUS_AUTH_TOKEN
+    //   }
+    // }
   ]
 };
